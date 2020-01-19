b13a3de89420ee886d9c6aa732b0bdbad8cf3c4d
Olaf Conijn <olaf_conijn@hotmail.com>
2020-01-19 23:42:51 +0100

    made help default command

--- cli.ts
+++ cli.ts
@@ -5,3 +5,7 @@ import { CliProgram } from './cli-program';
 const program = CliProgram.Create();
 const args = process.argv;
 program.parse(args);
+
+if (process.argv.length < 3) {
+    program.help();
+  }
