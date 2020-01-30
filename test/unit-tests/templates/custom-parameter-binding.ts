ab16acc80817aa69c98705e96db6ba5b47c8055c
Olaf Conijn <olaf_conijn@hotmail.com>
2020-01-30 21:43:54 +0100

    fix multiple targets found

--- test/unit-tests/templates/custom-parameter-binding.ts
+++ test/unit-tests/templates/custom-parameter-binding.ts
@@ -1,15 +1,10 @@
 import * as chai from 'chai';
 import { expect } from 'chai';
 import { CloudFormationBinder, ICfnBinding } from '../../../src/cfn-binder/cfn-binder';
-import { CfnTemplate } from '../../../src/cfn-binder/cfn-template';
-import { AccountResource } from '../../../src/parser/model/account-resource';
-import { CloudFormationResource } from '../../../src/parser/model/cloudformation-resource';
 import { OrgResourceTypes } from '../../../src/parser/model/resource-types';
-import { IResourceTarget } from '../../../src/parser/model/resources-section';
 import { TemplateRoot } from '../../../src/parser/parser';
 import { PersistedState } from '../../../src/state/persisted-state';
 import { ICfnTemplate } from '../cfn-types';
-import { TestTemplates } from '../test-templates';
 
 chai.use(require('chai-as-promised'));
 
@@ -38,5 +33,21 @@ describe('when specifying !Ref in custom parameter binding', () => {
         expect(account1CfnTemplate.Parameters.Param).to.not.be.undefined;
         expect(account1CfnTemplate.Parameters.Param.ExportAccountId).to.eq('444444444444');
     });
+});
 
+describe('when specifying !Ref in custom parameter binding to unknown account', () => {
+    it('exception is thrown', () => {
+        try {
+            const template = TemplateRoot.create('./test/resources/custom-parameter-binding/custom-parameter-binding-non-existant.yml');
+            const persistedState = PersistedState.CreateEmpty(template.organizationSection.masterAccount.accountId);
+            persistedState.setBinding({type: OrgResourceTypes.Account, physicalId: '111111111111', logicalId: 'Account1', lastCommittedHash: 'abc'});
+            persistedState.setBinding({type: OrgResourceTypes.Account, physicalId: '444444444444', logicalId: 'Account4', lastCommittedHash: 'abc'});
+
+            const cloudformationBinder = new CloudFormationBinder('custom-parameter-binding', template, persistedState);
+            cloudformationBinder.enumBindings();
+            expect.fail('expected exception');
+        } catch (err) {
+            expect(err.message).to.contain('UnkownAccount');
+        }
+    });
 });
