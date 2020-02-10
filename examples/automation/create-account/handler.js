753e9bd0cc17d0772e16b4967c5b9a91e1f2bc09
Olaf Conijn <olaf_conijn@hotmail.com>
2020-02-10 22:21:56 +0100

    added example on how to automate account creation process

--- examples/automation/create-account/handler.js
+++ examples/automation/create-account/handler.js
@@ -1,18 +1,45 @@
 'use strict';
+const AWS = require('aws-sdk');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+module.exports.getAccountData = async event => {
+  console.log({ log: 'getAccountData.input', ...event});
+
+  if (event === undefined) throw new Error('event must not be undefined');
+  if (event.accountId === undefined) throw new Error('event.accountId must not be undefined');
+
+  const org = new AWS.Organizations();
+  const response = await org.describeAccount({AccountId: event.accountId}).promise();
+
+  console.log({ log: 'getAccountData.response', ...response});
+
+  if (response.Account === undefined) throw new Error('unable to retrieve account');
+  return response.Account;
+};
+
+
+module.exports.sendEmail = async event => {
+  console.log({ log: 'sendEmail.input', ...event});
+
+  if (event === undefined) throw new Error('event must not be undefined');
+  if (event.contentTemplateName === undefined) throw new Error('event.contentTemplateName must not be undefined');
+  if (event.contentTemplateData === undefined) throw new Error('event.contentTemplateData must not be undefined');
+  if (event.toAddress === undefined) throw new Error('event.toAddress must not be undefined');
+
+  const ses = new AWS.SES();
+  const request = {
+    Destination: {
+      ToAddresses: [
+        event.toAddress
+      ]
+    },
+    Source: process.env.fromEmailAddress,
+    Template: event.contentTemplateName,
+    TemplateData: JSON.stringify(event.contentTemplateData)
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log({ log: 'sendEmail.request', ...request});
+  const response = await ses.sendTemplatedEmail(request).promise();
+
+  console.log({ log: 'sendEmail.response', ...response});
+  return response;
 };
