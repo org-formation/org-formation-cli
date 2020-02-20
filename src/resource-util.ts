9fa1c551f4e39bba76f4ab41099bb276fa81554a
Olaf Conijn <olaf_conijn@hotmail.com>
2020-02-20 22:52:17 +0100

    fix ResourceUtil date handling

--- src/resource-util.ts
+++ src/resource-util.ts
@@ -1,13 +1,15 @@
 import { bool } from 'aws-sdk/clients/signer';
 import { SubExpression } from './cfn-binder/cfn-sub-expression';
 
+const zeroPad = (num: number, places: number) => String(num).padStart(places, '0')
+
 export class ResourceUtil {
     public static FixVersions(obj: any) {
         if (obj !== null && typeof obj === 'object') {
             const entries = Object.entries(obj);
             for (const [key, val] of entries) {
                 if (key === 'Version' && val instanceof Date) {
-                    obj.Version = `${val.getFullYear()}-${val.getMonth()}-${val.getDate()}`;
+                    obj.Version = ResourceUtil.ToVersion(val);
                 } else if (key === 'Version' && typeof val === 'string' && val.endsWith('T00:00:00.000Z')) {
                     obj.Version = val.substring(0, val.indexOf('T'));
                 }
@@ -16,6 +18,13 @@ export class ResourceUtil {
                 }
             }
         }
+    }
+
+    public static ToVersion(date: Date) {
+        const year = date.getUTCFullYear();
+        const month = zeroPad(1 + date.getUTCMonth(), 2);
+        const day = zeroPad(date.getUTCDate(), 2);
+        return `${year}-${month}-${day}`;
     }
 
     public static HasExpressions(resourceParent: any, resourceKey: string, resourceIds: string[]): bool {
---
+++ test/unit-tests/resource-util.test.ts
@@ -0,0 +1,9 @@
+import { ResourceUtil } from "../../src/resource-util";
+
+describe('when fixing versions', () => {
+    test('dates are converted to string using UTC', ()=> {
+        const obj = { Version : new Date('2020-09-09T00:00:00.000Z') };
+        ResourceUtil.FixVersions(obj)
+        expect(obj.Version).toBe('2020-09-09');
+    });
+});
