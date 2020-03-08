e2daa382cc3d216a119eae1b4de3df2e5d07353b
Olaf Conijn <olaf_conijn@hotmail.com>
2020-03-08 21:28:08 +0100

    improved coverage on default template writer

--- test/unit-tests/writer/default-template-writer.test.ts
+++ test/unit-tests/writer/default-template-writer.test.ts
@@ -31,6 +31,67 @@ describe('when writing template for organization', () => {
         expect(defaultTemplate.state).toBeDefined();
     })
 
+    describe('and organization has accounts, ou\'s and scp\'s', () => {
+
+        beforeEach(() => {
+            organization.accounts = [
+                { Name: 'acc', Type: 'Account', Id: '123123123123', ParentId: 'o-root', Policies: []},
+            ];
+            organization.organizationalUnits = [
+                { Name: 'ou', Type: 'OrganizationalUnit', Id: 'ou-1', ParentId: 'o-root', Policies: [], Accounts: [], OrganizationalUnits: []}
+            ];
+            organization.policies = [
+                { Name: 'scp-aws-managed', Type: 'Policy', Id: 'scp-0', PolicySummary: {AwsManaged: true }, Content: '{}', Targets: [] },
+                { Name: 'scp', Type: 'Policy', Id: 'scp-1', PolicySummary: {AwsManaged: false }, Content: '{}', Targets: [] },
+            ];
+
+            organization.organizationalUnits[0].Accounts.push(organization.accounts[0]);
+            organization.organizationalUnits[0].Policies.push(organization.policies[1]);
+        })
+
+        test('template and state are generated successfully', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            expect(defaultTemplate.template).toBeDefined();
+            expect(defaultTemplate.state).toBeDefined();
+        })
+
+        test('generated template contains accounts', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            const root = TemplateRoot.createFromContents(defaultTemplate.template);
+            expect(root.organizationSection.accounts?.length).toBe(1);
+        });
+
+        test('generated template contains organizational unit', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            const root = TemplateRoot.createFromContents(defaultTemplate.template);
+            expect(root.organizationSection.organizationalUnits?.length).toBe(1);
+        });
+
+        test('generated template contains organizational unit to account relationship', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            const root = TemplateRoot.createFromContents(defaultTemplate.template);
+            const ou = root.organizationSection.organizationalUnits[0];
+            expect(ou.accounts?.length).toBe(1);
+            expect(ou.accounts[0].TemplateResource.logicalId).toBe(root.organizationSection.accounts[0].logicalId);
+        });
+
+        test('generated template contains organizational unit to scp relationship', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            const root = TemplateRoot.createFromContents(defaultTemplate.template);
+            const ou = root.organizationSection.organizationalUnits[0];
+            expect(ou.serviceControlPolicies?.length).toBe(1);
+            expect(ou.serviceControlPolicies[0].TemplateResource.logicalId).toBe(root.organizationSection.serviceControlPolicies[0].logicalId);
+        });
+
+
+        test('generated template only contains non AWS managed service control policy', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            const root = TemplateRoot.createFromContents(defaultTemplate.template);
+            expect(root.organizationSection.serviceControlPolicies?.length).toBe(1);
+            expect(root.organizationSection.serviceControlPolicies[0].policyName).toBe('scp');
+        });
+
+    });
     describe('and multiple accounts have the same name', () => {
 
         let consoleWarnMock: jest.SpyInstance;
@@ -57,10 +118,9 @@ describe('when writing template for organization', () => {
             expect(consoleWarnMock).toHaveBeenCalledWith(expect.stringContaining('abcdef'));
         })
 
-
         test('generated template contains both accounts', async () => {
             const defaultTemplate = await templateWriter.generateDefaultTemplate();
-            const root: TemplateRoot = TemplateRoot.createFromContents(defaultTemplate.template);
+            const root = TemplateRoot.createFromContents(defaultTemplate.template);
             expect(root.organizationSection.accounts).toBeDefined();
             expect(root.organizationSection.accounts.length).toBe(2);
         });
@@ -96,6 +156,37 @@ describe('when writing template for organization', () => {
             expect(root.organizationSection.accounts).toBeDefined();
             expect(root.organizationSection.accounts.length).toBe(1);
         });
+    });
+
+    describe('and multiple organizational units have the same name', () => {
+
+        beforeEach(() => {
+
+            organization.organizationalUnits = [
+                {Name: 'abcdef', Type: 'OrganizationalUnit', Id: 'ou-1', ParentId: 'o-root', Policies: [], Accounts: [], OrganizationalUnits: []},
+                {Name: 'abcdef', Type: 'OrganizationalUnit', Id: 'ou-2', ParentId: 'o-root', Policies: [], Accounts: [], OrganizationalUnits: []}
+            ];
+            organization.organizationalUnits[0].OrganizationalUnits.push(organization.organizationalUnits[1])
+        })
+
+        test('template and state are generated successfully', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            expect(defaultTemplate.template).toBeDefined();
+            expect(defaultTemplate.state).toBeDefined();
+        })
+
+        test('generated template contains parent/child relationship', async () => {
+            const defaultTemplate = await templateWriter.generateDefaultTemplate();
+            const root: TemplateRoot = TemplateRoot.createFromContents(defaultTemplate.template);
+            expect(root.organizationSection.organizationalUnits).toBeDefined();
+            expect(root.organizationSection.organizationalUnits.length).toBe(2);
+            expect(root.organizationSection.organizationalUnits.length).toBe(2);
+
+            const parent = root.organizationSection.organizationalUnits.find(x=>x.organizationalUnits.length > 0);
+            expect(parent).toBeDefined();
+            expect(parent.organizationalUnits?.length).toBe(1)
+        })
+
     });
 
     describe('and multiple organizational units have the same name', () => {
