b974db65d554506e9c7e8b6c72a2fd191dd821cc
Olaf Conijn <olaf_conijn@hotmail.com>
2020-04-09 00:13:00 +0200

    added some tests for exports as parameter

--- test/integration-tests/scenario-refence-export-as-parameter.test.ts
+++ test/integration-tests/scenario-refence-export-as-parameter.test.ts
@@ -1,6 +1,6 @@
-import { ValidateTasksCommand, PerformTasksCommand, DeleteStacksCommand } from "~commands/index";
+import { ValidateTasksCommand, PerformTasksCommand } from "~commands/index";
 import { readFileSync } from "fs";
-import { IIntegrationTestContext, baseBeforeAll, profileForIntegrationTests, sleepForTest, baseAfterAll } from "./base-integration-test";
+import { IIntegrationTestContext, baseBeforeAll, baseAfterAll } from "./base-integration-test";
 import { DescribeStacksOutput } from "aws-sdk/clients/cloudformation";
 
 const basePathForScenario = './test/integration-tests/resources/scenario-reference-export-as-parameter/';
@@ -15,15 +15,16 @@ describe('when importing value from another stack', () => {
         context = await baseBeforeAll();
 
         await context.prepareStateBucket(basePathForScenario + '0-state.json');
+        const { command, cfnClient } = context;
 
-        await ValidateTasksCommand.Perform({...context.command, tasksFile: basePathForScenario + '1-deploy-tasks-with-copy-value.yml' })
-        await PerformTasksCommand.Perform({...context.command, tasksFile: basePathForScenario + '1-deploy-tasks-with-copy-value.yml' });
+        await ValidateTasksCommand.Perform({...command, tasksFile: basePathForScenario + '1-deploy-tasks-with-copy-value.yml' })
+        await PerformTasksCommand.Perform({...command, tasksFile: basePathForScenario + '1-deploy-tasks-with-copy-value.yml' });
 
-        describedBucketStack = await context.cfnClient.describeStacks({StackName: 'scenario-export-bucket'}).promise();
-        describeBucketRoleStack = await context.cfnClient.describeStacks({StackName: 'scenario-export-bucket-role'}).promise();
+        describedBucketStack = await cfnClient.describeStacks({StackName: 'scenario-export-bucket'}).promise();
+        describeBucketRoleStack = await cfnClient.describeStacks({StackName: 'scenario-export-bucket-role'}).promise();
 
-        await PerformTasksCommand.Perform({...context.command, tasksFile: basePathForScenario + '2-cleanup-tasks-with-copy-value.yml', performCleanup: true });
-        const stacksAfterCleanup = await context.cfnClient.listStacks({StackStatusFilter: ['CREATE_COMPLETE', 'UPDATE_COMPLETE']}).promise();
+        await PerformTasksCommand.Perform({...command, tasksFile: basePathForScenario + '2-cleanup-tasks-with-copy-value.yml', performCleanup: true });
+        const stacksAfterCleanup = await cfnClient.listStacks({StackStatusFilter: ['CREATE_COMPLETE', 'UPDATE_COMPLETE']}).promise();
         expect(stacksAfterCleanup.StackSummaries.find(x=>x.StackName === 'scenario-export-bucket')).toBeUndefined();
         expect(stacksAfterCleanup.StackSummaries.find(x=>x.StackName === 'scenario-export-bucket-role')).toBeUndefined();
     });
@@ -31,10 +32,27 @@ describe('when importing value from another stack', () => {
     test('Stack parameter with CopyValue has value of Output', () =>{
         expect(describeBucketRoleStack).toBeDefined();
 
-        const parameter = describeBucketRoleStack.Stacks[0].Parameters[0];
+        const parameter = describeBucketRoleStack.Stacks[0].Parameters.find(x=>x.ParameterKey === 'bucketArn');
         const output = describedBucketStack.Stacks[0].Outputs[0];
-        expect(parameter.ParameterKey).toBe('bucketArn');
-        expect(output.ExportName).toBe('BucketName');
+        expect(output.ExportName).toBe('BucketArn');
+        expect(parameter.ParameterValue).toBe(output.OutputValue);
+    })
+
+    test('Stack parameter with explicit accountId has value of Output', () =>{
+        expect(describeBucketRoleStack).toBeDefined();
+
+        const parameter = describeBucketRoleStack.Stacks[0].Parameters.find(x=>x.ParameterKey === 'bucketArn2');
+        const output = describedBucketStack.Stacks[0].Outputs[0];
+        expect(output.ExportName).toBe('BucketArn');
+        expect(parameter.ParameterValue).toBe(output.OutputValue);
+    })
+
+    test('Stack parameter with explicit accountId and region has value of Output', () =>{
+        expect(describeBucketRoleStack).toBeDefined();
+
+        const parameter = describeBucketRoleStack.Stacks[0].Parameters.find(x=>x.ParameterKey === 'bucketArn3');
+        const output = describedBucketStack.Stacks[0].Outputs[0];
+        expect(output.ExportName).toBe('BucketArn');
         expect(parameter.ParameterValue).toBe(output.OutputValue);
     })
 
