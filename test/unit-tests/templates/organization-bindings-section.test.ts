57e173908645781dc30a838f66f81231108da492
Olaf Conijn <olaf_conijn@hotmail.com>
2020-12-05 01:41:54 +0100

    feat: null binding is treated as empty binding

--- test/unit-tests/templates/organization-bindings-section.test.ts
+++ test/unit-tests/templates/organization-bindings-section.test.ts
@@ -12,11 +12,11 @@ describe('when  using organization bindings section', () => {
         template = TemplateRoot.create('./test/resources/organization-bindings-section/organization-bindings-section.yml');
         const persistedState = PersistedState.CreateEmpty(template.organizationSection.masterAccount.accountId);
 
-        persistedState.setBinding({type: OrgResourceTypes.MasterAccount, physicalId: '000000000000', logicalId: 'MasterAccount', lastCommittedHash: 'abc'});
-        persistedState.setBinding({type: OrgResourceTypes.Account, physicalId: '111111111111', logicalId: 'Account1', lastCommittedHash: 'abc'});
-        persistedState.setBinding({type: OrgResourceTypes.Account, physicalId: '222222222222', logicalId: 'Account2', lastCommittedHash: 'abc'});
-        persistedState.setBinding({type: OrgResourceTypes.Account, physicalId: '333333333333', logicalId: 'Account3', lastCommittedHash: 'abc'});
-        persistedState.setBinding({type: OrgResourceTypes.Account, physicalId: '444444444444', logicalId: 'Account4', lastCommittedHash: 'abc'});
+        persistedState.setBinding({ type: OrgResourceTypes.MasterAccount, physicalId: '000000000000', logicalId: 'MasterAccount', lastCommittedHash: 'abc' });
+        persistedState.setBinding({ type: OrgResourceTypes.Account, physicalId: '111111111111', logicalId: 'Account1', lastCommittedHash: 'abc' });
+        persistedState.setBinding({ type: OrgResourceTypes.Account, physicalId: '222222222222', logicalId: 'Account2', lastCommittedHash: 'abc' });
+        persistedState.setBinding({ type: OrgResourceTypes.Account, physicalId: '333333333333', logicalId: 'Account3', lastCommittedHash: 'abc' });
+        persistedState.setBinding({ type: OrgResourceTypes.Account, physicalId: '444444444444', logicalId: 'Account4', lastCommittedHash: 'abc' });
 
         cloudformationBinder = new CloudFormationBinder('organization-bindings-section', template, persistedState);
         bindings = await cloudformationBinder.enumBindings();
@@ -65,5 +65,42 @@ describe('when  using organization bindings section', () => {
         expect(resource3.normalizedForeachAccounts).toBeDefined();
         expect(resource3.normalizedForeachAccounts.length).toBe(5);
 
+    });
+
+    test('Resource 4 has binding to only master and foreach on 0', () => {
+        const resource4 = template.resourcesSection.resources.find((x) => x.logicalId === 'Resource4');
+        expect(resource4.normalizedBoundAccounts).toBeDefined();
+        expect(resource4.normalizedBoundAccounts.length).toBe(1);
+        expect(resource4.normalizedBoundAccounts[0]).toBe('MasterAccount');
+        expect(resource4.normalizedForeachAccounts).toBeDefined();
+        expect(resource4.normalizedForeachAccounts.length).toBe(0);
+
+    });
+
+    test('no resource 4 in processed template', () => {
+        const masterBinding = bindings.find(x => x.accountLogicalId === 'MasterAccount');
+        const template = masterBinding.template.createTemplateBody();
+        const parsedTemplate = JSON.parse(template) as ITemplate;
+        const keys = Object.keys(parsedTemplate.Resources);
+        const resource4 = keys.find(x => x.startsWith('Resource4'));
+        expect(resource4).toBeUndefined();
+    });
+
+    test('Resource 5 has binding to only master and foreach on 0', () => {
+        const resource5 = template.resourcesSection.resources.find((x) => x.logicalId === 'Resource5');
+        expect(resource5.normalizedBoundAccounts).toBeDefined();
+        expect(resource5.normalizedBoundAccounts.length).toBe(1);
+        expect(resource5.normalizedBoundAccounts[0]).toBe('MasterAccount');
+        expect(resource5.normalizedForeachAccounts).toBeDefined();
+        expect(resource5.normalizedForeachAccounts.length).toBe(0);
+
+    });
+    test('no resource 5 in processed template', () => {
+        const masterBinding = bindings.find(x => x.accountLogicalId === 'MasterAccount');
+        const template = masterBinding.template.createTemplateBody();
+        const parsedTemplate = JSON.parse(template) as ITemplate;
+        const keys = Object.keys(parsedTemplate.Resources);
+        const resource5 = keys.find(x => x.startsWith('Resource5'));
+        expect(resource5).toBeUndefined();
     });
 });
