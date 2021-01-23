0f0569f6867b2d46d2a506a808bb6485fa865881
Khai Do <zaro0508@gmail.com>
2021-01-22 22:27:13 -0800

    [Resolves #152] URL reference for ReadFile function
    Add the ability to get files from a url reference.

--- src/core/cfn-functions/cfn-read-file.ts
+++ src/core/cfn-functions/cfn-read-file.ts
@@ -2,18 +2,48 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import { ICfnFunctionContext } from './cfn-functions';
 import { OrgFormationError } from '~org-formation-error';
+const fs = require('fs')
+const url = require('url');
+const { schema } = require('yaml-cfn');
+const fetch = require('node-fetch');
+const yaml = require('js-yaml');
+
+async function fetchAndDecode(urlRef: string) {
+    let response = await fetch(urlRef);
+    let content;
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    else {
+        const urlPath = url.parse(urlRef, true).pathname;
+        const fileExt = path.extname(urlPath);
+        if (fileExt === '.json') {
+            content = await response.json();
+        } else if (fileExt === '.yaml' || fileExt === '.yml') {
+            let reeText = await response.text();
+            content = yaml.load(reeText, { schema: schema })
+        } else {
+            content = await response.text();
+        }
+        return content;
+    }
+}
 
 export class CfnReadFile {
 
-    static resolve(context: ICfnFunctionContext, resource: any, resourceParent: any, resourceKey: string, key: string, val: any): void {
+    static async resolve(context: ICfnFunctionContext, resource: any, resourceParent: any, resourceKey: string, key: string, val: any): Promise<void> {
         if (key === 'Fn::ReadFile')
         {
             if (typeof val !== 'string') {
                 if (!context.finalPass) { return; }
                 throw new OrgFormationError(`Fn::ReadFile expression expects a string as value. Found ${typeof val}`);
             }
-
-            const resolved = CfnReadFile.readFile(context.filePath, val);
+            let resolved;
+            if (val.startsWith('http')) {
+                resolved = await fetchAndDecode(val);
+            } else {
+                resolved = CfnReadFile.readFile(context.filePath, val);
+            }
             resourceParent[resourceKey] = resolved;
         }
     }
