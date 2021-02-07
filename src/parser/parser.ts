2fdc1c7242a80425e4fe179e479a15de1a11f1f6
Olaf Conijn <olaf_conijn@hotmail.com>
2021-02-07 11:27:09 +0100

    fix: allow SAM globals section in template #180 (#183)

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -29,6 +29,7 @@ export interface ITemplate {
     Metadata?: any;
     Parameters?: any;
     Mappings?: any;
+    Globals?: any;
     Conditions?: any;
     Resources?: IResources;
     Outputs?: any;
