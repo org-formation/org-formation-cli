efdc07d035ab7630d8ac5235a389ddbd6244ee0a
Khai Do <zaro0508@gmail.com>
2021-02-10 10:23:33 -0800

    run command with await

--- src/core/cfn-functions/cfn-cmd.ts
+++ src/core/cfn-functions/cfn-cmd.ts
@@ -5,7 +5,7 @@ import { OrgFormationError } from '~org-formation-error';
 
 export class CfnCmd {
 
-    static resolve(context: ICfnFunctionContext, resource: any, resourceParent: any, resourceKey: string, key: string, val: any): void {
+    static async resolve(context: ICfnFunctionContext, resource: any, resourceParent: any, resourceKey: string, key: string, val: any): Promise<string> {
         if (key === 'Fn::Cmd')
         {
             if (typeof val !== 'string') {
@@ -13,7 +13,7 @@ export class CfnCmd {
                 throw new OrgFormationError(`Fn::Cmd expression expects a string as value. Found ${typeof val}`);
             }
 
-            const resolved = CfnCmd.run(context.filePath, val);
+            const resolved = await CfnCmd.run(context.filePath, val);
             resourceParent[resourceKey] = resolved;
         }
     }
