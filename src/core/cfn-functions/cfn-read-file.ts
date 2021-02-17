d8df778678011e0fdbf63380caeecf7d93122c84
Khai Do <zaro0508@gmail.com>
2021-02-16 21:45:37 -0800

    use methods from file-util.ts

--- src/core/cfn-functions/cfn-read-file.ts
+++ src/core/cfn-functions/cfn-read-file.ts
@@ -1,56 +1,26 @@
-import { readFileSync } from 'fs';
 import path from 'path';
 import { ICfnFunctionContext } from './cfn-functions';
 import { OrgFormationError } from '~org-formation-error';
-const url = require('url');
-const { schema } = require('yaml-cfn');
-const fetch = require('node-fetch');
-const yaml = require('js-yaml');
-
-async function fetchAndDecode(urlRef: string) {
-    let response = await fetch(urlRef);
-    let content;
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    else {
-        const urlPath = url.parse(urlRef, true).pathname;
-        const fileExt = path.extname(urlPath);
-        if (fileExt === '.json') {
-            let resObj = await response.json();
-            content = JSON.stringify(resObj);
-        } else if (fileExt === '.yaml' || fileExt === '.yml') {
-            let resObj = await response.text();
-            content = yaml.dump(resObj, { schema: schema })
-        } else {
-            content = await response.text();
-        }
-        return content;
-    }
-}
+import { FileUtil } from '~util/file-util';
 
 export class CfnReadFile {
 
-    static async resolve(context: ICfnFunctionContext, resource: any, resourceParent: any, resourceKey: string, key: string, val: any): Promise<void> {
+    static async resolve(context: ICfnFunctionContext, resource: any, resourceParent: any, resourceKey: string, key: string, val: any): Promise<string> {
         if (key === 'Fn::ReadFile')
         {
             if (typeof val !== 'string') {
                 if (!context.finalPass) { return; }
                 throw new OrgFormationError(`Fn::ReadFile expression expects a string as value. Found ${typeof val}`);
             }
-            let resolved;
-            if (val.startsWith('http')) {
-                resolved = await fetchAndDecode(val);
-            } else {
-                resolved = CfnReadFile.readFile(context.filePath, val);
+
+            let resolvedFilePath = val;
+            if (!FileUtil.IsRemoteFile(val)) {
+                const dir = path.dirname(context.filePath);
+                resolvedFilePath = path.resolve(dir, val);
             }
+
+            const resolved = await FileUtil.GetContents(resolvedFilePath);
             resourceParent[resourceKey] = resolved;
         }
-    }
-
-    static readFile(contextPath: string, filePath: string): string {
-        const dir = path.dirname(contextPath);
-        const resolvedFilePath = path.resolve(dir, filePath);
-        return readFileSync(resolvedFilePath).toString('utf-8');
     }
 }
