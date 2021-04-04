ba7fd7da555ecdffccde0209d4a1e1783792a69d
Olaf Conijn <olaf_conijn@hotmail.com>
2021-04-04 14:36:43 +0200

    Chore/fix integration test (#239)
    * doc: added doc about 0.9.15 breaking change
    
    * doc: added some more on the 0.9.15 breaking change
    
    * chore: fix integration test deploy custom roles

--- src/cfn-binder/cfn-validate-task-provider.ts
+++ src/cfn-binder/cfn-validate-task-provider.ts
@@ -7,6 +7,7 @@ import { CfnExpressionResolver } from '~core/cfn-expression-resolver';
 import { TemplateRoot } from '~parser/parser';
 import { PersistedState } from '~state/persisted-state';
 import { AwsUtil, CfnUtil } from '~util/aws-util';
+import { ConsoleUtil } from '~util/console-util';
 
 export class CfnValidateTaskProvider {
     constructor(private readonly template: TemplateRoot, private readonly state: PersistedState, private readonly logVerbose: boolean) {
@@ -36,7 +37,7 @@ export class CfnValidateTaskProvider {
             descriptionsOfBoundParameters.push(param.Description);
         }
 
-        const expressionResolver = CfnExpressionResolver.CreateDefaultResolver(binding.accountLogicalId, binding.accountId, binding.region, binding.customRoleName,  binding.customViaRoleArn, this.template.organizationSection, this.state, false);
+        const expressionResolver = CfnExpressionResolver.CreateDefaultResolver(binding.accountLogicalId, binding.accountId, binding.region, binding.customRoleName, binding.customViaRoleArn, this.template.organizationSection, this.state, false);
         const stackName = await expressionResolver.resolveSingleExpression(binding.stackName, 'StackName');
 
         return {
@@ -46,36 +47,45 @@ export class CfnValidateTaskProvider {
             isDependency: (): boolean => false,
             action: 'Validate',
             perform: async (): Promise<void> => {
-                const customRoleName = await expressionResolver.resolveSingleExpression(binding.customRoleName, 'CustomRoleName');
-                const customViaRoleArn = await expressionResolver.resolveSingleExpression(binding.customViaRoleArn, 'CustomViaRoleArn');
+                try {
+                    const customRoleName = await expressionResolver.resolveSingleExpression(binding.customRoleName, 'CustomRoleName');
+                    const customViaRoleArn = await expressionResolver.resolveSingleExpression(binding.customViaRoleArn, 'CustomViaRoleArn');
 
-                const templateBody = await binding.template.createTemplateBodyAndResolve(expressionResolver, true);
-                const cfn = await AwsUtil.GetCloudFormation(binding.accountId, binding.region, customRoleName, customViaRoleArn);
+                    const templateBody = await binding.template.createTemplateBodyAndResolve(expressionResolver, true);
+                    const cfn = await AwsUtil.GetCloudFormation(binding.accountId, binding.region, customRoleName, customViaRoleArn);
 
-                const validateInput: ValidateTemplateInput =  {
-                    TemplateBody: templateBody,
-                };
+                    const validateInput: ValidateTemplateInput = {
+                        TemplateBody: templateBody,
+                    };
 
-                await CfnUtil.UploadTemplateToS3IfTooLarge(validateInput, binding, stackName, this.template.hash);
+                    await CfnUtil.UploadTemplateToS3IfTooLarge(validateInput, binding, stackName, this.template.hash);
 
 
-                const result = await cfn.validateTemplate(validateInput).promise();
-                const missingParameters: string[] = [];
-                for (const param of result.Parameters) {
+                    const result = await cfn.validateTemplate(validateInput).promise();
+                    const missingParameters: string[] = [];
+                    for (const param of result.Parameters) {
 
-                    if (param.DefaultValue !== undefined) {
-                        continue;
+                        if (param.DefaultValue !== undefined) {
+                            continue;
+                        }
+                        if (binding.parameters && (binding.parameters[param.ParameterKey] !== undefined)) {
+                            continue;
+                        }
+                        if (descriptionsOfBoundParameters.includes(param.Description)) {
+                            continue;
+                        }
+                        missingParameters.push(param.ParameterKey);
                     }
-                    if (binding.parameters && (binding.parameters[param.ParameterKey] !== undefined)) {
-                        continue;
+                    if (missingParameters.length > 0) {
+                        throw new OrgFormationError(`template expects parameter(s) ${missingParameters.join(', ')} which have not been provided`);
                     }
-                    if (descriptionsOfBoundParameters.includes(param.Description)) {
-                        continue;
+                } catch (err) {
+                    if (err.code === 'AccessDenied') {
+                        ConsoleUtil.LogWarning(`access denied when running validate stack: ${err}`);
+                        return;
                     }
-                    missingParameters.push(param.ParameterKey);
-                }
-                if (missingParameters.length > 0)  {
-                    throw new OrgFormationError(`template expects parameter(s) ${missingParameters.join(', ')} which have not been provided`);
+                    throw err;
+
                 }
             },
         };
