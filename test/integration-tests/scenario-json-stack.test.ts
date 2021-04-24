2c2ac7cc84566054acfa92b0ba715e5da8b0423f
Olaf Conijn <olaf_conijn@hotmail.com>
2021-04-24 12:35:14 +0200

    teat: added integration test for deploy of json stack (without version header)

---
+++ test/integration-tests/scenario-json-stack.test.ts
@@ -0,0 +1,35 @@
+import { PerformTasksCommand, ValidateTasksCommand } from '~commands/index';
+import { IIntegrationTestContext, baseBeforeAll } from './base-integration-test';
+import { DescribeStacksOutput } from 'aws-sdk/clients/cloudformation';
+
+const basePathForScenario = './test/integration-tests/resources/scenario-json-template/';
+
+describe('when calling org-formation perform tasks', () => {
+    let context: IIntegrationTestContext;
+    let jsonStack: DescribeStacksOutput;
+
+    beforeAll(async () => {
+
+        context = await baseBeforeAll();
+        await context.prepareStateBucket(basePathForScenario + '../state.json');
+        const command = context.command;
+        const { cfnClient } = context;
+
+        await ValidateTasksCommand.Perform({ ...command, tasksFile: basePathForScenario + '1-deploy-json-stack.yml' })
+        await PerformTasksCommand.Perform({ ...command, tasksFile: basePathForScenario + '1-deploy-json-stack.yml' });
+
+        jsonStack = await cfnClient.describeStacks({ StackName: 'test-with-json' }).promise();
+
+
+        await PerformTasksCommand.Perform({ ...command, tasksFile: basePathForScenario + '9-cleanup-json-stack.yml', performCleanup: true });
+
+    });
+
+    test('stack was deployed successfully', () => {
+        expect(jsonStack).toBeDefined();
+        expect(jsonStack.Stacks.length).toBe(1);
+        expect(jsonStack.Stacks[0]).toBeDefined();
+        expect(jsonStack.Stacks[0].StackStatus).toBe('CREATE_COMPLETE');
+    });
+
+});
