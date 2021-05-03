32bd5a0daae2517f7d858de051109a71c05a8494
Olaf Conijn <olaf_conijn@hotmail.com>
2021-05-03 20:59:27 +0200

    fix: fail on wrong condition checking for Definitions

--- src/build-tasks/build-configuration.ts
+++ src/build-tasks/build-configuration.ts
@@ -144,8 +144,8 @@ export class BuildConfiguration {
         delete buildFile.Parameters;
         delete buildFile.Mappings;
 
-        if ((buildFile.Definitions as any).Type) {
-            throw new OrgFormationError('tasks file should not have task called `Definitions`. this attribute is reserved for yaml anchors. Did Definitions somehow declare a Type attribute? use an array within your Definitions to declare yaml anchors.');
+        if (buildFile.Definitions && (buildFile.Definitions as any).Type) {
+            throw new OrgFormationError('Tasks file should not have a task called `Definitions`. This Definitions attribute is reserved for yaml anchors. Did Definitions somehow declare a Type attribute? use an array within your Definitions to declare yaml anchors.');
         }
 
         delete buildFile.Definitions;
