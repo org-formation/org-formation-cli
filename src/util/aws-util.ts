5a1d9a02ba4e1d362c1a62948d6ff5ddeadd57e4
Olaf Conijn <olaf_conijn@hotmail.com>
2021-05-27 22:33:39 +0200

    fix: not retrow error when creating stack

--- src/util/aws-util.ts
+++ src/util/aws-util.ts
@@ -375,8 +375,9 @@ export class CfnUtil {
                     if (innerErr && innerErr.code === 'ValidationError' && innerErr.message && innerErr.message.indexOf('does not exist') !== -1) {
                         await cfn.createStack(updateStackInput).promise();
                         describeStack = await cfn.waitFor('stackCreateComplete', { StackName: updateStackInput.StackName, $waiter: { delay: 1, maxAttempts: 60 * 30 } }).promise();
+                    } else {
+                        throw innerErr;
                     }
-                    throw innerErr;
                 }
             } catch (err) {
                 if (err && (err.code === 'OptInRequired' || err.code === 'InvalidClientTokenId')) {
