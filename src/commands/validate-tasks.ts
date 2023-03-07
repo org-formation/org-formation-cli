b051d1230cf772541804241a63938b0996cf0b58
Mihail Chirosca <mihail@silverflow.com>
2023-03-07 08:07:00 +0100

    feat: added match to validate command

--- src/commands/validate-tasks.ts
+++ src/commands/validate-tasks.ts
@@ -1,10 +1,12 @@
 import { Command } from 'commander';
+import minimatch from 'minimatch';
 import { BaseCliCommand } from './base-command';
 import { IPerformTasksCommandArgs } from './perform-tasks';
-import { BuildConfiguration } from '~build-tasks/build-configuration';
+import { BuildConfiguration, IBuildTask } from '~build-tasks/build-configuration';
 import { BuildRunner } from '~build-tasks/build-runner';
 import { Validator } from '~parser/validator';
 import { AwsUtil } from '~util/aws-util';
+import { ConsoleUtil } from '~util/console-util';
 
 const commandName = 'validate-tasks <tasksFile>';
 const commandDescription = 'Will validate the tasks file, including configured tasks';
@@ -33,6 +35,7 @@ export class ValidateTasksCommand extends BaseCliCommand<IPerformTasksCommandArg
         command.option('--debug-templating [debug-templating]', 'when set to true the output of text templating processes will be stored on disk', false);
         command.option('--large-template-bucket-name [large-template-bucket-name]', 'bucket used when uploading large templates. default is to create a bucket just-in-time in the target account');
         command.option('--dev', 'use development settings, e.g. DefaultDevelopmentBuildAccessRoleName instead of DefaultBuildAccessRoleName', false);
+        command.option('--match [match]', 'glob pattern used to define/filter which tasks to run.');
 
         super.addOptions(command);
     }
@@ -56,6 +59,44 @@ export class ValidateTasksCommand extends BaseCliCommand<IPerformTasksCommandArg
 
         await config.fixateOrganizationFile(command);
         const validationTasks = config.enumValidationTasks(command);
+
+        if (command.match) {
+            const skippedTasks = this.skipNonMatchingLeafTasks(validationTasks, command.match, '');
+            if (skippedTasks === validationTasks.length) {
+                ConsoleUtil.LogWarning(`--match parameter glob '${command.match}' did not match any tasks. Use --verbose to see the tasks it did not match`);
+            }
+        }
+
         await BuildRunner.RunValidationTasks(validationTasks, command.verbose === true, command.maxConcurrentTasks, command.failedTasksTolerance);
+    }
+
+    private skipNonMatchingLeafTasks(tasks: IBuildTask[], taskMatcher: string, tasksPrefix: string): number {
+        let skippedTasks = 0;
+        for (const task of tasks) {
+
+            const isLeafTask = task.childTasks.length === 0;
+            const taskFullName = `${tasksPrefix}${task.name}`;
+
+            if (isLeafTask) {
+                const isMatching = task.name === taskMatcher || minimatch(taskFullName, taskMatcher);
+                task.skip = isMatching ? false : true;
+            } else {
+                const skippedChildTasks = this.skipNonMatchingLeafTasks(task.childTasks, taskMatcher, `${taskFullName}/`);
+                const isAllSkipped = task.childTasks.length === skippedChildTasks;
+                task.skip = isAllSkipped ? true : false;
+            }
+
+            if (task.skip) {
+                skippedTasks = skippedTasks + 1;
+            }
+
+            if (isLeafTask && task.skip !== true) {
+                ConsoleUtil.LogInfo(`${taskFullName} matched the '${taskMatcher}' globPattern`);
+            } else {
+                ConsoleUtil.LogDebug(`${taskFullName} did not match the '${taskMatcher}' globPattern`);
+            }
+
+        }
+        return skippedTasks;
     }
 }
