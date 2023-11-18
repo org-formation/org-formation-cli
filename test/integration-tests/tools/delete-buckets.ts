d8b5280c110118d46aa2ccdfcd939e3c40f94164
Olaf Conijn <olaf_conijn@hotmail.com>
2023-11-19 00:01:11 +0100

    chore: improve delete-buckets to delete delete-markers

--- test/integration-tests/tools/delete-buckets.ts
+++ test/integration-tests/tools/delete-buckets.ts
@@ -1,18 +1,22 @@
-import { DeleteBucketCommand, DeleteObjectCommand, ListBucketsCommand, ListObjectsCommand, S3Client } from '@aws-sdk/client-s3';
+import { DeleteBucketCommand, DeleteObjectCommand, ListBucketsCommand, ListObjectVersionsCommand, ListObjectsCommand, ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3';
 import { fromIni } from '@aws-sdk/credential-providers';
 
 const bucketsToDelete = /(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}/;
 
 const deleteBuckets = async (): Promise<void> => {
-    const s3 = new S3Client({ credentials: fromIni({ profile: 'org-formation-test-v2' }) });
+    const s3 = new S3Client({ credentials: fromIni({ profile: 'org-formation-test-v2' }), region: "eu-west-1" });
     const bucketList = await s3.send(new ListBucketsCommand({}));
     for (const bucket of bucketList.Buckets) {
+
         if (bucketsToDelete.test(bucket.Name)) {
             console.log(`deleting ${bucket.Name}`);
             try {
-                const objectList = await s3.send(new ListObjectsCommand({ Bucket: bucket.Name }));
-                for (const obj of objectList.Contents) {
-                    await s3.send(new DeleteObjectCommand({ Bucket: bucket.Name, Key: obj.Key }));
+                const objectVersionList = await s3.send(new ListObjectVersionsCommand({ Bucket: bucket.Name }));
+                for (const obj of (objectVersionList.Versions || [])) {
+                    await s3.send(new DeleteObjectCommand({ Bucket: bucket.Name, Key: obj.Key, VersionId: obj.VersionId }));
+                }
+                for (const obj of (objectVersionList.DeleteMarkers || [])) {
+                    await s3.send(new DeleteObjectCommand({ Bucket: bucket.Name, Key: obj.Key, VersionId: obj.VersionId }));
                 }
                 await s3.send(new DeleteBucketCommand({ Bucket: bucket.Name }));
             } catch (err) {
